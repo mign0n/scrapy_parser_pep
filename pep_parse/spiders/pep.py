bee3265bf35bee216cf9e9ac80e64aafd13de980
mign0n <mign0n.git@gmail.com>
2023-12-19 10:54:03 +0500

    feat: Add PEPs parser.

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['http://peps.python.org/']
+
+    def parse(self, response):
+        for pep_link in response.css('section#numerical-index a[href^="pep-"]'):
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        name = response.css('section#pep-content h1::text')
+        number, *_ = name.re(r'\d+')
+        yield PepParseItem(
+            {
+                'number': number,
+                'name': name.get(),
+                'status': response.css('section#pep-content abbr::text').get(),
+            }
+        )
