13457f8de445263d38ead0083569b1a6fb30d62a
mign0n <mign0n.git@gmail.com>
2023-12-20 14:25:05 +0500

    refactor: Remove unneeded dict.

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,3 +1,5 @@
+import re
+
 import scrapy
 
 from pep_parse.items import PepParseItem
@@ -15,12 +17,9 @@ class PepSpider(scrapy.Spider):
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
-        name = response.css('section#pep-content h1::text')
-        number, *_ = name.re(r'\d+')
+        name = response.css('section#pep-content h1::text').get()
         yield PepParseItem(
-            {
-                'number': number,
-                'name': name.get(),
-                'status': response.css('section#pep-content abbr::text').get(),
-            }
+            name=name,
+            number=re.search(r'\d+', name).group(0),
+            status=response.css('section#pep-content abbr::text').get(),
         )
