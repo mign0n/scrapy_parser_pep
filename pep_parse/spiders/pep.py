6bf18f60b82352e0d12a3aa61f2d915600605560
mign0n <mign0n.git@gmail.com>
2023-12-20 13:49:20 +0500

    refactor: Eliminate code duplication.

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -6,7 +6,7 @@ from pep_parse.items import PepParseItem
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['https://peps.python.org/']
+    start_urls = ['https://{}/'.format(domain) for domain in allowed_domains]
 
     def parse(self, response):
         for pep_link in response.css(
